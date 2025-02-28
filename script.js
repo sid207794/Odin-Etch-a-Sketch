4bb6c2712032fa8d3e06370357bd6df6de517e7b
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-02-28 17:09:18 +0000

    Rearrange order of button and container constants, copy the for loop from inside the event handler to just before it and replace power function with number 256 and change height of element to 640/16, finally add replaceChildren() attribute inside event handler

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const body = document.querySelector("body");
 
-const parent = document.createElement("div");
-parent.setAttribute("id", "container");
-
 const button = document.createElement("button");
 button.textContent = "Grid Size";
 button.style.marginBottom = "15px";
@@ -10,22 +7,40 @@ button.style.padding = "5px 20px";
 button.style.fontSize = "15px";
 button.style.borderRadius = "10px";
 
+body.appendChild(button);
+
+const parent = document.createElement("div");
+parent.setAttribute("id", "container");
+body.appendChild(parent);
+
+const container = document.querySelector("#container");
+
+for (i=0; i<256; i++) {
+    const element = document.createElement("div");
+    element.setAttribute("class", "square");
+    element.setAttribute("style", `height: ${640/16}px`)
+    element.addEventListener("mouseover", () => {
+        element.style.background = "black";
+    });
+    
+    container.appendChild(element);
+}
+
 button.addEventListener("click", function gridSize() {
     do {
         userInput = parseInt(prompt("Enter the grid size: "));
     } while (userInput > 100);
-    
-    const container = document.querySelector("#container");
 
+    container.replaceChildren();
+    
     for (i=0; i<Math.pow(userInput, 2); i++) {
         const element = document.createElement("div");
         element.setAttribute("class", "square");
+        element.setAttribute("style", `height: ${640/userInput}px`)
         element.addEventListener("mouseover", () => {
             element.style.background = "black";
         });
+        
         container.appendChild(element);
     }
 });
-
-body.appendChild(button);
-body.appendChild(parent);
