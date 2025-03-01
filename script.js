d7850fcbdd203f05191afd3e365aee93058d194d
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-03-01 09:35:25 +0000

    Add body and container background color, add more styles to button

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const body = document.querySelector("body");
 
+body.style.background = "rgb(30, 30, 30)";
+
 const button = document.createElement("button");
-button.textContent = "Grid Size";
+button.textContent = "GRID SIZE";
 button.style.marginBottom = "15px";
 button.style.padding = "5px 20px";
 button.style.fontSize = "15px";
+button.style.fontWeight = "bold";
 button.style.borderRadius = "10px";
+button.style.background = "rgb(180, 180, 180)";
+button.style.borderColor = "white";
 
 body.appendChild(button);
 
@@ -14,6 +19,7 @@ parent.setAttribute("id", "container");
 body.appendChild(parent);
 
 const container = document.querySelector("#container");
+container.style.background = "black";
 
 for (i=0; i<256; i++) {
     const element = document.createElement("div");
@@ -26,7 +32,7 @@ for (i=0; i<256; i++) {
         const COLOR_GREEN = Math.floor(Math.random()*256);
         const COLOR_BLUE = Math.floor(Math.random()*256);
         element.style.background = `rgb(${COLOR_RED}, ${COLOR_GREEN}, ${COLOR_BLUE})`;
-        
+
         if (j <= 0) {
             element.style.filter = `brightness(0%)`;
         } else {
